7c28972bcca0f3a45c4c99fa1c6726cceba81ebb
Julien Plée <julplee@gmail.com>
2020-02-28 16:42:04 +0100

    refactor code

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/otiai10/copy"
 )
+
+var root = "./settings-to-copy"
 
 type config struct {
 	UserPath      string   `json:"user_path"`
@@ -18,7 +19,6 @@ type config struct {
 func main() {
 	var configFiles []string
 
-	root := "./settings-to-copy"
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -26,8 +26,6 @@ func main() {
 
 		if filepath.Base(path) == "backup-config.json" {
 			configFiles = append(configFiles, path)
-		} else {
-			fmt.Println(filepath.Base(path))
 		}
 
 		return nil
@@ -37,28 +35,43 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(configFiles)
 	for _, configFile := range configFiles {
-		fmt.Println(configFile)
-		file, err := ioutil.ReadFile(configFile)
+		copyFollowingConfigFile(configFile)
+	}
+
+}
+
+func copyFollowingConfigFile(configFile string) error {
+	file, err := ioutil.ReadFile(configFile)
+
+	if err != nil {
+		return err
+	}
+
+	data := config{}
+
+	_ = json.Unmarshal([]byte(file), &data)
+
+	for _, folderToSave := range data.FoldersToSave {
+		folderToSave = filepath.Join(data.UserPath, folderToSave)
+		backupFolder := root + "/backup"
+
+		err := copyFolderToBackupFolder(folderToSave, backupFolder)
 
 		if err != nil {
-			panic(err)
-		}
-
-		data := config{}
-
-		_ = json.Unmarshal([]byte(file), &data)
-
-		for _, el := range data.FoldersToSave {
-			path := filepath.Join(data.UserPath, el)
-
-			err := copy.Copy(path, root+"/copy")
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("copy")
+			return err
 		}
 	}
 
+	return nil
+}
+
+func copyFolderToBackupFolder(folderToSave string, backupFolder string) error {
+	err := copy.Copy(folderToSave, backupFolder)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
